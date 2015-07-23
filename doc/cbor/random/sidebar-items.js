1bf29460220ecf470855adb1da4ad27d7692dbf3
Toralf Wittner <tw@dtex.org>
2015-07-23 12:05:15 +0200

    Provide documentation.

---
+++ doc/cbor/random/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["gen_value","Generate a random `cbor::Value`. Mostly useful for quickcheck related tests. The parameter `level` denotes the maximum nesting of this value."]]});
---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['byteorder'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='byteorder/enum.Error.html' title='byteorder::Error'>Error</a>",];implementors['log'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='log/struct.SetLoggerError.html' title='log::SetLoggerError'>SetLoggerError</a>",];implementors['cbor'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='cbor/decoder/enum.DecodeError.html' title='cbor::decoder::DecodeError'>DecodeError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='cbor/encoder/enum.EncodeError.html' title='cbor::encoder::EncodeError'>EncodeError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
